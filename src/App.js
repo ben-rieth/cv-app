e9c150b6376dc5003a08cf6c5257938ea62ba3ea
ben-rieth <benrieth3@gmail.com>
2022-05-22 15:35:23 -0500

    Reset branch

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import FlexibleInput from "./components/FlexibleInput";
+import FlexibleTextArea from "./components/FlexibleTextArea";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <FlexibleInput id="first-name" name="first-name" placeholder="First Name" fontSize={1.75} maxWidth={300} />
+        <FlexibleTextArea placeholder="Objective or Summary"/>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
