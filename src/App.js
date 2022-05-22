2ab73dc13a5833c2200f324374851e9000f873a7
ben-rieth <benrieth3@gmail.com>
2022-05-22 15:42:36 -0500

    Add header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@
 import React from "react";
 
 import FlexibleInput from "./components/FlexibleInput";
-import FlexibleTextArea from "./components/FlexibleTextArea";
+
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <FlexibleInput id="first-name" name="first-name" placeholder="First Name" fontSize={1.75} maxWidth={300} />
-        <FlexibleTextArea placeholder="Objective or Summary"/>
+      <div className="content">
+        <div className="header-container">
+          <h1 className="header">CV Builder</h1>
+        </div>
       </div>
-      
     );
   }
 }
