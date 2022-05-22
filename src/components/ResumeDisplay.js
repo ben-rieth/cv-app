0399cd3fb228406225eb70d4e1e8939946eb13fb
ben-rieth <benrieth3@gmail.com>
2022-05-22 16:09:19 -0500

    Add horizontal margin to ResumeDisplay

--- src/components/ResumeDisplay.js
+++ src/components/ResumeDisplay.js
@@ -6,6 +6,7 @@ import Sidebar from "./resume/Sidebar";
 import Main from "./resume/Main";
 
 const DisplayContainer = styled.div`
+    margin: 0 5vw;
     display: grid;
     grid-template-columns: 1fr 2fr;
     grid-template-rows: 1fr 5fr;
