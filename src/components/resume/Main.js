cf2d82e9cdd52f5aaea289230a4b1da12bd1794a
ben-rieth <benrieth3@gmail.com>
2022-05-23 16:02:31 -0500

    Can add sections from options dropdown

--- src/components/resume/Main.js
+++ src/components/resume/Main.js
@@ -8,27 +8,57 @@ const MainContainer = styled.div`
 `;
 
 class Main extends React.Component {
+
+    dropdownOptions = [
+        {
+            optionName: "Education",
+            id: 1,
+            onSelectOption: () => this.createSection("Education")
+        },
+        {
+            optionName: "Work Experience",
+            id: 2,
+            onSelectOption: () => this.createSection("Work Experience")
+        },
+        {
+            optionName: "Projects",
+            id: 3,
+            onSelectOption: () => this.createSection("Projects")
+        },
+        {
+            optionName: "Achievements",
+            id: 4,
+            onSelectOption: () => this.createSection("Achievements")
+        }
+    ]
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sections: [],
+            options: this.dropdownOptions
+        }
+
+        this.createSection = this.createSection.bind(this);
+    }
+
+    createSection(sectionType) {
+        this.setState({
+            sections: this.state.sections.concat(<h1 key={sectionType}>{sectionType}</h1>),
+            options: this.state.options.filter(option => option.optionName !== sectionType)
+        });
+    }
+
     render() {
+        
+        const {sections, options} = this.state;
+
         return(
             <MainContainer>
+                {sections}
                 <AddSectionDropdown 
-                    options={[
-                        {
-                            optionName: "Education",
-                            id: 1,
-                            onSelectOption: () => alert("Education")
-                        },
-                        {
-                            optionName: "Work Experience",
-                            id: 2,
-                            onSelectOption: () => alert("Work")
-                        },
-                        {
-                            optionName: "Projects",
-                            id: 3,
-                            onSelectOption: () => alert("Projects")
-                        }
-                    ]}
+                    options={options}
                 />
             </MainContainer>
         );
