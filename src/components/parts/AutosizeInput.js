91fb96e3dae37a8d14b1a4f477524ddfbe6d5892
ben-rieth <benrieth3@gmail.com>
2022-05-23 21:32:27 -0500

    Add focus state property for input

--- src/components/parts/AutosizeInput.js
+++ src/components/parts/AutosizeInput.js
@@ -21,22 +21,20 @@ const Placeholder = styled.div`
 
 const ValidityIcon = styled.img`
     position: absolute;
-    left: calc(${props => props.distance}ch + 5px);
+    left: min(calc(${props => props.distance}ch + 5px), 95%);
     pointer-events: none;
     width: ${props => props.iconWidth}rem;
     filter: invert(96%) sepia(81%) saturate(3047%) hue-rotate(79deg) brightness(96%) contrast(112%);
 `;
 
 const Input = styled.span`
-    min-width: calc(${props => props.len}ch + ${props=>props.spaceForIcon}rem);
-    max-width: 100%;
+    width: calc(${props => props.len}ch + ${props=>props.spaceForIcon}rem + 5px);
     border: none;
     outline: none;
-    border-bottom: 2px solid lightgrey;
     padding: 0 0 2px 0;
     white-space: nowrap;
     overflow: hidden;
-
+    border-bottom: 2px solid lightgrey;
     &:focus {
         border-bottom: 2px solid dodgerblue;
     }
@@ -49,7 +47,8 @@ class AutosizeInput extends React.Component {
         
         this.state = {
             value: this.props.initialValue,
-            placeholderShown: this.props.initialValue === "" ? true : false
+            placeholderShown: this.props.initialValue === "" ? true : false,
+            focused: false
         };
 
         this.onInputChange = this.onInputChange.bind(this);
@@ -102,8 +101,20 @@ class AutosizeInput extends React.Component {
         this.stopIfPastCharacterMax(event);
     }
 
+    onFocus() {
+        this.setState({
+            focused: true
+        })
+    } 
+
+    onBlur() {
+        this.setState({
+            focused: false
+        })
+    }
+
     render() {
-        const {placeholder, fontSize} = this.props;
+        const {placeholder, fontSize, icon} = this.props;
         const {value, placeholderShown, matchesPattern} = this.state;
 
         let elementLength = value.length >= placeholder.length ? value.length : placeholder.length;
@@ -119,11 +130,13 @@ class AutosizeInput extends React.Component {
                     suppressContentEditableWarning="true"
                     onInput={this.onInputChange}
                     onKeyDown={this.onKeyDown}
-                    autoFocus />
+                    onFocus={this.onFocus} 
+                    onBlur={this.onBlur}
+                    />
                 <Placeholder className="label">
                     {placeholderShown ? placeholder : ''}
                 </Placeholder>
-                {matchesPattern && !placeholderShown ? 
+                {matchesPattern && !placeholderShown && icon  ? 
                     <ValidityIcon src={CheckCircleIcon} iconWidth={fontSize} distance={elementLength}/> : <div></div>}
             </InputWrapper>
         );
@@ -136,7 +149,8 @@ AutosizeInput.defaultProps = {
     fontSize: 1,
     initialValue: "",
     characterLimit: 100,
-    pattern: /[\s\S]*/
+    pattern: /[\s\S]*/,
+    icon: true
 }
 
 export default AutosizeInput;
