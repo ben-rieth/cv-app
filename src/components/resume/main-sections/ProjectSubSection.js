cb6af5d8b454e274fe329fc4de413e6a810b4e8d
ben-rieth <benrieth3@gmail.com>
2022-05-25 17:00:57 -0500

    Add EditableBulletList to project subsection

--- src/components/resume/main-sections/ProjectSubSection.js
+++ src/components/resume/main-sections/ProjectSubSection.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AutosizeInput from "../../parts/AutosizeInput";
+import EditableBulletList from "../../parts/EditableBulletList";
 
 class ProjectSubSection extends React.Component {
     render() {
         return (
             <div className="subsection-form">
                 <AutosizeInput placeholder="Project Name" fontSize={1.2} />
+                <EditableBulletList placeholder="Description of Achievement/Contribution" fontSize={.8}/>
             </div>
         )
     }
